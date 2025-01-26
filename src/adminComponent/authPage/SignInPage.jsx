ffdc0529f645a69a76092c0d5f2064caa7fc94ad
Manish-sikar <141156129+Manish-sikar@users.noreply.github.com>
2025-01-26 20:30:32 +0530

    Update SignInPage.jsx

--- src/adminComponent/authPage/SignInPage.jsx
+++ src/adminComponent/authPage/SignInPage.jsx
@@ -34,14 +34,6 @@ const SinInPage = () => {
           <div className="col col-xl-10">
             <div className="card" style={{ borderRadius: "1rem" }}>
               <div className="row g-0">
-                <div className="col-md-6 col-lg-5 d-none d-md-block">
-                  <img
-                    src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/img1.webp"
-                    alt="login form"
-                    className="img-fluid"
-                    style={{ borderRadius: "1rem 0 0 1rem" }}
-                  />
-                </div>
                 <div className="col-md-6 col-lg-7 d-flex align-items-center">
                   <div className="card-body p-4 p-lg-5 text-black">
                     <form onSubmit={handleLogin}>
