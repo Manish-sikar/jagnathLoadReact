899cf64a5936ec443ea88f623f66fa9964ee815e
Manish-sikar <141156129+Manish-sikar@users.noreply.github.com>
2025-01-26 13:07:52 +0530

    Update applyFormData.jsx

--- src/adminComponent/adminComponent/userApplyForm/applyFormData.jsx
+++ src/adminComponent/adminComponent/userApplyForm/applyFormData.jsx
@@ -32,14 +32,6 @@ const ApplyFormData = () => {
   return (
     <div className="col-md-12 mt-5">
       <div className="card">
-        <div className="card-header">
-          <div className="d-flex align-items-center">
-            <h4 className="card-title">Partner's</h4>
-            <Link to="/admin/become-partner" className="btn btn-primary btn-round ms-auto">
-              <i className="fa fa-plus"></i> Add Partner
-            </Link>
-          </div>
-        </div>
         <div className="card-body">
           <div className="table-responsive">
             {loading ? (
