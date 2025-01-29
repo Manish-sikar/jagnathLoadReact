6b2fbef86559a5bdf7c4e949dc899538ded73a0f
Manish-sikar <141156129+Manish-sikar@users.noreply.github.com>
2025-01-29 23:52:25 +0530

    Update Header.jsx

--- src/component/common/Header.jsx
+++ src/component/common/Header.jsx
@@ -68,7 +68,7 @@ const Header = () => {
         <Container>
           <Navbar.Brand href="#home" style={style.navbarBrand}>
           <img 
-        src={`${baseURL}/${formData?.site_logo}`} 
+        src={formData?.site_logo}
         alt="Site Logo" 
         className="rounded-circle" 
         style={{ width: 'auto', height: '80px' }}  // Adjust size of the logo as needed
