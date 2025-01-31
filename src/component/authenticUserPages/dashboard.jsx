c5ef6991f5423f6dc0cf83e933cd451063f97321
Manish-sikar <141156129+Manish-sikar@users.noreply.github.com>
2025-02-01 01:46:41 +0530

    Update dashboard.jsx

--- src/component/authenticUserPages/dashboard.jsx
+++ src/component/authenticUserPages/dashboard.jsx
@@ -1,21 +1,37 @@
-import LoginBanner from "../bannerPages/loginBanner";
-import Service from "../Service";
+import { useEffect, useState } from "react";
 import AuthUserHeader from "./headerUser";
 import SubHeaderUser from "./subHeader";
+import LoanProductList from "./loanProduct";
+import { GetLoanData } from "../../services/loanDataServices";
+
 const Dashboard = () => {
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState(null);
+
+  const fetchProductsByCategory = async(category) => {
+const response = await GetLoanData()
+const productsData = response?.loan_Data || []
+    const filteredData = productsData.filter((product) => product.category === category);
+    setFilteredProducts(filteredData);
+    setSelectedCategory(category);
+  };
+
+  useEffect(()=>{
+    fetchProductsByCategory()
+  },[])
+
   return (
     <>
-    <AuthUserHeader></AuthUserHeader>
-     <SubHeaderUser></SubHeaderUser>
-      {/* start Banner */}
-      <LoginBanner/>
-      {/* end Banner */}
-      {/* start services */}
-      <Service></Service>
-      {/* end services */}
+      <AuthUserHeader />
+      <SubHeaderUser onCategorySelect={fetchProductsByCategory} />
+      <div className="container mt-4">
+        <h4>{selectedCategory ? selectedCategory.replace("_", " ").toUpperCase() : "Select a Category"}</h4>
+        <LoanProductList products={filteredProducts} />
+      </div>
     </>
   );
 };
+
 export default Dashboard;
 
-
+ 
