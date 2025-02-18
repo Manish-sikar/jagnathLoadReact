23ce136138f7185c847ee2652a72bbb8450f5d14
swatikulshreshtha <kulshreshtha9853@gmail.com>
2025-02-18 22:51:15 +0530

    Update Service.jsx

--- src/component/Service.jsx
+++ src/component/Service.jsx
@@ -41,7 +41,7 @@ const Service = () => {
             320: { slidesPerView: 1, spaceBetween: 10 },  // Mobile
             640: { slidesPerView: 3, spaceBetween: 15 }, // Small tablets
             768: { slidesPerView: 4, spaceBetween: 20 }, // Tablets
-            1024: { slidesPerView: 6, spaceBetween: 20 }, // Laptops
+            1024: { slidesPerView: 6, spaceBetween: 20 },
             1280: { slidesPerView: 8, spaceBetween: 15 }, // Desktops
           }}
           style={{ width: "100%", padding: "0" }}
