979540af2b1d3b862b7188666c633964212f8b36
Manish-sikar <141156129+Manish-sikar@users.noreply.github.com>
2025-04-15 08:43:53 +0530

    Update subHeader.jsx

--- src/component/authenticUserPages/subHeader.jsx
+++ src/component/authenticUserPages/subHeader.jsx
@@ -1,9 +1,8 @@
-import { useNavigate } from "react-router-dom";
-import { Navbar, Nav, NavDropdown, Offcanvas, Container } from "react-bootstrap";
+
+
+import React from "react";
 
 const SubHeaderUser = ({ onCategorySelect }) => {
-  const navigate = useNavigate();
-
   const dropdownData = [
     { title: "Loan Products", category: "loan_product" },
     { title: "Our Services", category: "our_service" },
@@ -11,33 +10,30 @@ const SubHeaderUser = ({ onCategorySelect }) => {
     { title: "Account Opening", category: "account_opening" },
     { title: "Investment", category: "investment" },
     { title: "Insurance", category: "insurance" },
-    { title: "Estore", category: "e_store" },
+    { title: "E-Store", category: "e_store" },
     { title: "Report", category: "report" },
     { title: "Support", category: "support" },
   ];
 
   return (
-    <Navbar collapseOnSelect expand="lg" style={{ backgroundColor: "#3674B5", height: "40px" }} variant="dark">
-      <Container fluid>
-        <Navbar.Toggle aria-controls="offcanvasNavbar" />
-        <Navbar.Offcanvas id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel" placement="end">
-          <Offcanvas.Header closeButton>
-            <Offcanvas.Title id="offcanvasNavbarLabel">Categories</Offcanvas.Title>
-          </Offcanvas.Header>
-          <Offcanvas.Body>
-          <Nav className="d-flex justify-content-center w-100">
+   
 
-              {dropdownData.map((dropdown, index) => (
-                <Nav.Link key={index} onClick={() => onCategorySelect(dropdown.category)}>
-                  {dropdown.title}
-                </Nav.Link>
-              ))}
-            </Nav>
-          </Offcanvas.Body>
-        </Navbar.Offcanvas>
-      </Container>
-    </Navbar>
+    <nav class="navbar bg-body-tertiary">
+  <form class="container-fluid justify-content-start">
+  {dropdownData.map((dropdown, index) => (
+            <div
+              key={index}
+              className="sub-header-item text-white text-center"
+              onClick={() => onCategorySelect(dropdown.category)}
+            >
+    <button class="btn btn-outline-success me-2 bg-dark mx-1" type="button">{dropdown.title}</button>
+
+    </div>
+          ))}
+  </form>
+</nav>
   );
 };
 
 export default SubHeaderUser;
+
